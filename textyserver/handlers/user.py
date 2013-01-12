f548b44bf554736ff041467d4c58b53f5e9f4fa8
Samuel Lucidi <slucidi@newstex.com>
2013-01-12 03:53:13 -0800

    Properly serializing requests.post request

--- textyserver/handlers/user.py
+++ textyserver/handlers/user.py
@@ -76,15 +76,14 @@ class UserHandler(RequestHandler):
 		return response
 
 def getLiveConnectTokens(auth_code):
-	base_url = "https://login.live.com/oauth20_token.srf?"
+	base_url = "https://login.live.com/oauth20_token.srf"
 	params = {
 		"grant_type" : "authorization_code",
 		"redirect_url" : "https://api.buildanavy.com/user/token",
 		"client_id": textyserver.CLIENT_ID,
 		"client_secret": textyserver.CLIENT_SECRET
 	}
-	live_connect_url = base_url + urllib.urlencode(params)
-	return requests.post(live_connect_url, headers={"content-type":"application/x-www-form-urlencoded"}).json()
+	return requests.post(base_url, data=params, headers={"content-type":"application/x-www-form-urlencoded"}).json()
 
 def createUser(params):
 	required_params = ["email", "phone", "auth_token", "refresh_token"]
