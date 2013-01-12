14b2ef08abbd3dbde9e476cfa501fa58b9dbda75
Samuel Lucidi <slucidi@newstex.com>
2013-01-12 02:26:20 -0800

    Added rudimentary skydrive login support

--- textyserver/__init__.py
+++ textyserver/__init__.py
@@ -1,1 +1,6 @@
+import boto
+
 __version__ = "0.1.0"
+
+CLIENT_ID = boto.config.get("Skydrive", "client_id")
+CLIENT_SECRET = boto.config.get("Skydrive", "client_secret")
--- textyserver/handlers/user.py
+++ textyserver/handlers/user.py
@@ -4,31 +4,43 @@ from textyserver.resources.user import TextyUser
 import logging
 import json
 import boto
+import urllib
+import requests
+import textyserver
 
 log = logging.getLogger('texty.userHandler')
 
 class UserHandler(RequestHandler):
 
 	def _get(self, request, response, id=None):
-		param = id.split('/')[0]
-		# if param == "code":
-		# 	code = request.params["code"]
+		params = id.split('/')
+		if params[0] == "code":
+			log.info(request)
+			auth_code = request.params["code"]
+			email = request.params["email"]
+			response = getLiveConnectTokens(auth_code)
+			user = TextyUser.find(email=email).next()
+			user.auth_token = response["access_token"]
+			user.refresh_token = response["refresh_token"]
+			user.put()
+			try:
+				challenge = getChallengeCode()
+			except Exception:
+				# retry 
+				raise
+			try:
+				user.sms(challenge)
+			except Exception:
+				# deal with twilio errors
+				raise
+		return response
 
 	def _post(self, request, response, id=None):
 		response.content_type = "application/json"
+		params = id.split('/')
 
 		user = createUser(request.params)
-		try:
-			challenge = getChallengeCode()
-		except Exception:
-			# retry 
-			raise
-
-		try:
-			user.sms(challenge)
-		except Exception:
-			# deal with twilio errors
-			raise
+		requestLiveConnectCode(user.email)
 
 		response.body = json.dumps(user.to_dict()) 
 		return response
@@ -51,6 +63,36 @@ class UserHandler(RequestHandler):
 		user.put()
 
 		return response
+
+def getLiveConnectTokens(auth_code):
+	client_id=CLIENT_ID&redirect_uri=REDIRECT_URI&client_secret=CLIENT_SECRET&code=AUTHORIZATION_CODE&grant_type=authorization_code
+	base_url = "https://login.live.com/oauth20_token.srf?"
+	params = {
+		"grant_type" : "authorization_code",
+		"redirect_url" : "https://api.buildanavy.com/user/token",
+		"client_id": textyserver.CLIENT_ID,
+		"client_secret": textyserver.CLIENT_SECRET
+	}
+	live_connect_url = base_url + urllib.urlencode(params)
+	return json.loads(requests.post(live_connect_url).json())
+
+def requestLiveConnectCode(email):
+	base_url = "https://login.live.com/oauth20_authorize.srf?"
+	params = {
+		"response_type" : "code",
+		"redirect_url" : "https://api.buildanavy.com/user/code",
+		"scopes" : " ".join([
+			"wl.basic",
+			"wl.signin",
+			"wl.offline_access",
+			"wl.skydrive",
+			"wl.skydrive_update"
+		]),
+		"client_id": textyserver.CLIENT_ID,
+		"email": email
+	}
+	live_connect_url = base_url + urllib.urlencode(params)
+	return requests.get(live_connect_url)
 
 def createUser(params):
 	required_params = ["email", "token", "number"]
