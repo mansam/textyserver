cfb0573f2c5ba8718d71d23a7c9502746d33f1da
Samuel Lucidi <slucidi@newstex.com>
2013-01-11 18:21:10 -0800

    Log twilio message params

--- textyserver/handlers/twilio.py
+++ textyserver/handlers/twilio.py
@@ -4,9 +4,13 @@ log = logging.getLogger('texty.twilioHandler')
 
 class TwilioHandler(RequestHandler):
 
+
+	sqs = boto.connect_sqs()
+	text_queue = sqs.lookup("")
+
 	def _post(self, request, response, id=None):
 		response.content_type = "text/plain"
-		log.info(request.body)
+		log.info(request.params)
 		response.body = "Message received."
 		return response
 
