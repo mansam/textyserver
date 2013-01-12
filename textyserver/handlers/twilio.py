464684eb5b29100f87e78da84346d6253edf0056
Samuel Lucidi <slucidi@newstex.com>
2013-01-11 17:12:24 -0800

    fixed request logging and response to twilio

--- textyserver/handlers/twilio.py
+++ textyserver/handlers/twilio.py
@@ -5,8 +5,8 @@ log = logging.getLogger('texty.twilioHandler')
 class TwilioHandler(RequestHandler):
 
 	def _post(self, request, response, id=None):
-		response.content_type = "application/json"
-		print(request.body)
+		response.content_type = "text/plain"
+		log.info(request.body)
 		response.body = "Message received."
 		return response
 
