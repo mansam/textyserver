7ca77c7086d19bcccfa35750767063db85af6fd7
Will Stephenson <william_stephenson@brown.edu>
2013-01-11 23:34:17 -0500

    Modified Worker.py

--- bin/worker.py
+++ bin/worker.py
@@ -5,11 +5,21 @@ import boto
 import logging
 import signal
 import json
+import skydrive
+from skydrive import api_v5, conf
+
 
 class Worker(multiprocessing.Process):
 
-	def __init__(self):
+	def __init__(self, client_id, client_secret):
 		super(Worker, self).__init__()
+
+		#Create and set up the connection to SkyDrive
+		#client_secret and client_id do not change from user to user
+		self.sd = skydrive.api_v5.SkyDriveAPI()
+		self.sd.client_id = client_id
+		self.sd.client_secret = client_secret
+
 		self.sqs = boto.connect_sqs()
 		self.text_queue = self.sqs.lookup('texts')
 		self.log = logging.getLogger('texty.workers')
@@ -21,13 +31,26 @@ class Worker(multiprocessing.Process):
 
 			msg = self.text_queue.read()
 			if msg:
+				#Consists of a phone number and a text message body
 				body = json.loads(msg.get_body())
 				phone_num = body[0]
 				txt = body[1]
+				split_txt = txt.split(' ')
+				
+				if split_txt[0] == 'get' and len(split_txt) == 2:
+					results = traverse(split_txt)
+					if len(results['fileNames']) == 1:
+						return_msg = results['fileNames'][0]
+
+				else:
+					return_msg = 'Error: Command not found or incorrectly formated'
 				try:
 					user = TextyUser.find(phone=phone_num).next()
+					sd.auth_access_token = user.auth_token #also user.refresh_token
+
+					#if confirmation code, set the user to active user.is_active = True and user.put()
 					try:
-						user.sms('Got your message.')
+						user.sms(return_msg)
 					except:
 						# failed to send message
 						self.log.exception('Failed sending sms to %s.' % phone_num)
@@ -38,6 +61,19 @@ class Worker(multiprocessing.Process):
 				
 			else:
 				self.log.info('Waiting.')
+
+
+	#Traverses the SkyDrive, starting from the location given by path.
+	def traverse(path):
+		ls = sd.listdir(path)
+		for a in range(len(ls)):
+			if ls[a]['type'] == u'folder':
+				traverse(ls[a]['id'])
+			elif ls[a]['name'].find(searchTerm) != -1:
+				filesFound.append(ls[a]['name'])
+				filesFoundIDs.append(ls[a]['id'])
+		return {'fileNames':filesFound, 'fileIDs':filesFoundIDs}
+
 
 if __name__ == "__main__":
 
