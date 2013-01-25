5fd63755e2aa455923462d06388b621c5d37043a
Samuel Lucidi <slucidi@newstex.com>
2013-01-24 21:11:56 -0500

    POST controller can now take care of redirecting the user to Live auth

--- textyserver/handlers/user.py
+++ textyserver/handlers/user.py
@@ -1,10 +1,10 @@
 from botoweb.appserver.handlers import RequestHandler
-from botoweb.exceptions import BadRequest, NotFound, TemporaryRedirect
+from botoweb.exceptions import BadRequest, NotFound, TemporaryRedirect, Conflict
 from textyserver.resources.user import TextyUser
 import logging
 import json
 import boto
-import requests
+import urllib
 import textyserver
 import liveconnect.skydrive 
 
@@ -51,17 +51,35 @@ class UserHandler(RequestHandler):
 
 	def _post(self, request, response, id=None):
 		response.content_type = "application/json"
-		challenge = request.params["challenge"]
-		try:
-			user = TextyUser.find(challenge=challenge).next()
-		except StopIteration:
-			log.exception('Invalid challenge code: %s' % challenge)
-			raise NotFound("Invalid challenge code.")
-		user.is_active = True
-		user.put()
-		user.sms('Thanks for signing up with Texty! Text HLP for commands.')
+		if id:
+			params = id.split('/')
+			if params and params[0] == "new":
+				try:
+					email = request.params["email"]
+					phone = request.params["phone"]
+					user = TextyUser.find(phone=phone).next()
+				except StopIteration:
+					scopes = ['wl.basic', 'wl.skydrive', 'wl.skydrive_update', 'wl.offline_access']
+					auth_url = sd.generate_auth_url(scopes=scopes,
+													redirect_uri=textyserver.REDIRECT_URI,
+													state="%s %s" % (phone, email))
+					raise TemporaryRedirect(auth_url)
+				except:
+					raise BadRequest("Missing required fields.")
+				else:
+					raise Conflict("Phone number already in use.")
+		else:
+			challenge = request.params["challenge"]
+			try:
+				user = TextyUser.find(challenge=challenge).next()
+			except StopIteration:
+				log.exception('Invalid challenge code: %s' % challenge)
+				raise NotFound("Invalid challenge code.")
+			user.is_active = True
+			user.put()
+			user.sms('Thanks for signing up with Texty! Text HLP for commands.')
 
-		response.body = json.dumps(user.to_dict())
+			response.body = json.dumps(user.to_dict())
 		return response
 
 	def _put(self, request, response, id=None):
